075ccfa2163f36d56e8e0f7e3dda9fa357caa3d4
ethanfinkel <ethanfinkel98@gmail.com>
2018-09-07 18:25:37 -0700

    added comments

--- Problem test.py
+++ Problem test.py
@@ -4,15 +4,15 @@ sys.setrecursionlimit(100000)
 x = 0
 y= 0
 sum = x+y
-safeset =set()
-treeset = set()
+safeset =set() #this is the set of walkable coridantes
+treeset = set() #coridantes where there are trees
 
 #d is down u is up l is left and r is right
 
-def sum_check (x,y):
+def sum_check (x,y): # this checks to see if the space is a tree or not
     xstr = x
     ystr = y
-    if (x<0):
+    if (x<0): #makes absolute value
         xstr = x*-1
     if (y<0):
         ystr = y*-1
@@ -20,52 +20,52 @@ def sum_check (x,y):
     sx = str(xstr)
     sy = str(ystr)
     sum =0
-    while i < len(sx):
+    while i < len(sx): #adds up the sum of the digits for x
         string_store = sx[i]
         x1 = abs(int (string_store))
         sum = sum + x1
         i += 1
     i=0
-    while i < len(sy):
+    while i < len(sy): #adds up the sum of the digits for y
         string_store = sy[i]
         y1 = abs(int (string_store))
         sum = sum + y1
         i += 1
-    if (sum<=21):
+    if (sum<=21): #checks if coridantes are safe and adds them if they are
         safeset.add((x,y))
         return True
 
-    else:
+    else: # adds coridantes to treeset if they are trees
         treeset.add((x,y))
         return False
 
-def check(x,y,safeset):
+def check(x,y,safeset): #this checks if the space has already been checked before
     if (x,y) in safeset:
         return False
     else:
         return True
 
 
-def move (x,y,count):
+def move (x,y,count): #the recursive function to move
     print (count)
     print (x,y)
-    if check(x, y + 1, safeset) is True and sum_check(x,y+1) is True :
+    if check(x, y + 1, safeset) is True and sum_check(x,y+1) is True : #this checks if the space above has already been checked and if there is a tree there
         count +=1
         print ("f")
-        move (x,y + 1,count)
-    if check(x + 1, y, safeset) is True  and sum_check(x+1,y) is True :
+        move (x,y + 1,count) #if there is no tree and the space has not been checked then the program moves forward and starts recursion
+    if check(x + 1, y, safeset) is True  and sum_check(x+1,y) is True :# same going right
         count +=1
         print ("r")
         move (x + 1,y,count)
-    if check(x - 1, y, safeset) is True  and sum_check(x-1,y) is True:
+    if check(x - 1, y, safeset) is True  and sum_check(x-1,y) is True: # same going left
         count +=1
         print ("l")
         move (x - 1,y,count)
-    if check(x, y - 1, safeset) is True and sum_check(x,y-1) is True:
+    if check(x, y - 1, safeset) is True and sum_check(x,y-1) is True: #same going down
         count +=1
         print ("d")
         move (x,y - 1,count)
 
 
 
-move (0,0,1)
+move (0,0,1) #initializes the function

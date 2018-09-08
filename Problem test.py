7cbeaf9841ba20860b15ebbb80ebb0cc2c52d36e
ethanfinkel <ethanfinkel98@gmail.com>
2018-09-07 18:14:39 -0700

    HOLY SHIT I GOT IT

--- Problem test.py
+++ Problem test.py
@@ -1,19 +1,24 @@
+import sys
+
+sys.setrecursionlimit(100000)
 x = 0
 y= 0
 sum = x+y
-safeset =set ()
+safeset =set()
 treeset = set()
 
 #d is down u is up l is left and r is right
 
 def sum_check (x,y):
+    xstr = x
+    ystr = y
     if (x<0):
-        x = x*-1
+        xstr = x*-1
     if (y<0):
-        y=y*-1
+        ystr = y*-1
     i=0
-    sx = str(x)
-    sy = str(y)
+    sx = str(xstr)
+    sy = str(ystr)
     sum =0
     while i < len(sx):
         string_store = sx[i]
@@ -28,7 +33,6 @@ def sum_check (x,y):
         i += 1
     if (sum<=21):
         safeset.add((x,y))
-
         return True
 
     else:
@@ -45,36 +49,23 @@ def check(x,y,safeset):
 def move (x,y,count):
     print (count)
     print (x,y)
-    if sum_check(x,y+1) is True and check(x,y+1,safeset) is True:
+    if check(x, y + 1, safeset) is True and sum_check(x,y+1) is True :
         count +=1
-        y +=1
         print ("f")
-        print (x,y)
-        move (x,y,count)
-    else:
-        if sum_check(x+1,y) is True and check(x+1,y,safeset) is True:
-            count +=1
-            x+= 1
-            print ("r")
-            move (x,y,count)
-        else:
-            if sum_check(x-1,y) is True and check(x-1,y,safeset) is True:
-                count +=1
-                x = x-1
-                print ("l")
-                move (x,y,count)
-            else:
-                if sum_check(x,y-1) is True and check(x,y-1,safeset) is True:
-                    count +=1
-                    y = y-1
-                    print ("d")
-                    move (x,y,count)
+        move (x,y + 1,count)
+    if check(x + 1, y, safeset) is True  and sum_check(x+1,y) is True :
+        count +=1
+        print ("r")
+        move (x + 1,y,count)
+    if check(x - 1, y, safeset) is True  and sum_check(x-1,y) is True:
+        count +=1
+        print ("l")
+        move (x - 1,y,count)
+    if check(x, y - 1, safeset) is True and sum_check(x,y-1) is True:
+        count +=1
+        print ("d")
+        move (x,y - 1,count)
 
 
 
 move (0,0,1)
-
-
-
-for z in range (0,len(treeset)):
-    print (treeset[z],safeset[z])
